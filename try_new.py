fc69050352e64b73bba10c8cc82bd4bd7761ab83
cxfl <fenglinggod@qq.com>
2024-03-23 04:37:42 +0800

    add some try

--- try_new.py
+++ try_new.py
@@ -1,3 +1,8 @@
+"""代码的一些新尝试"""
+
+from kuai_log import k_logger
+
+
 def check_args(params, data, json):
     w = 8
     arguments = locals()
@@ -17,6 +22,29 @@ def check_dict():
         print(i, key, value)
 
 
+
+def derector_log(func):
+    def wrapper(**kwargs):
+        args = locals()
+        args.pop('func')
+        print(args)
+        end_args = args['kwargs']
+        print(end_args)
+        for i, (arg_key, arg_value) in enumerate(end_args.items(), start=1):
+            if arg_value is not None:
+                output = str(arg_value)
+            k_logger.info(f"{arg_key}为：" + output)
+        return func(**kwargs)
+    return wrapper
+
+
+@derector_log
+def hhh(a=None, b=None, c=None, d=None):
+    e = 3
+    print(e)
+    return f"val = {e}"
+
+
 if __name__ == '__main__':
     # try_locals = check_args(params=None, data='hhh', json=3)
-    check_dict()
+    hhh(a=1, b=2, c=4)
